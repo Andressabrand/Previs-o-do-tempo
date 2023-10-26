1601e045f31bf07a1c7ad52657d2bee2791cf3f0
andressa <andbrandsym@gmail.com>
2023-10-26 10:39:05 -0300

    Projeto map redunce e filter

--- README.md
+++
@@ -1,3 +0,0 @@
-# Previs-o-do-tempo
-<p>has  Desenvolvi esse projeto com o intuito de desenvolver minhas habilidades coom algumas ferramentas do JavaScript, são elas: APIs e Math.floor. Com a ajuda de ums API, consigo buscar informações em um servidor, para informar-nos qual a temperatura de cada estado em tempo real.</p>
-<p> Espero que gostem !</p>
--- script.js
+++ script.js
@@ -1,41 +1,61 @@
+const showAllButton = document.querySelector('.show-all')
+const mapButton = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')
+const filter = document.querySelector('.filter-vegan')
+const list = document.querySelector('.list')
 
-
-
-const key = "96906b28136a7be294c9c7dc5dc20c5d"
-
-
-function ColocarDadosNaTela(dados){
-    console.log(dados)
-document.querySelector(".cidade").innerHTML= "Tempo em " + dados.name
-document.querySelector(".temp").innerHTML= Math.floor(dados.main.temp) + "°C"
-document.querySelector(".texto-previsão").innerHTML = dados.weather[0].
-description
-document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
-document.querySelector(".img-previsão").scr = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
-
-
-
-
+function format(value){
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(value)
 }
 
 
 
+function showAll(items){
 
- async function BuscarCidade( cidade){
-const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then (resposta => resposta.json()) 
+    let newLi = ''
 
+    items.forEach(item => {
+        newLi = newLi + `
+            <li>
+                <img src="${item.src}">
+                <p>${item.name}</p>
+                <p class="price">${format(item.price)}</p>
+            </li>
+        `
+    })
 
-ColocarDadosNaTela(dados)
-
+    list.innerHTML = newLi
 }
 
+function mapAll(){
+    const newPrice = menuOptions.map(item => ({
+        ...item,
+        price: item.price * 0.9,
+    }))
 
-
-
-
-function CliqueiNoBotao(){
-const cidade = document.querySelector(".input-city").value
-
-BuscarCidade ( cidade)
-
+    showAll(newPrice)
 }
+
+function sumAllItems(){
+    const totalValue = menuOptions.reduce( (acc, cur) => {
+        return acc + cur.price
+    }, 0)
+
+    list.innerHTML = `
+        <li>A soma de todos os items do menu: <p class="price">${format(totalValue)}</p></li>
+    `
+}
+
+function filterJustVegan(){
+    const justVegan = menuOptions.filter(item => item.vegan)
+
+    showAll(justVegan)
+}
+
+showAllButton.addEventListener('click', () => showAll(menuOptions))
+mapButton.addEventListener('click', mapAll)
+sumAll.addEventListener('click', sumAllItems)
+filter.addEventListener('click', filterJustVegan)
